854298602d5e9125562cd8553fb6d6b28e17660c
00012670 <wiut00012670@gmail.com>
2023-11-04 16:20:28 +0500

    add DataContext class

---
+++ API/DAL/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> o) : base(o)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Habit> Habits { get; set; }
+    }
+}
