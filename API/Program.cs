b1ef5d635597c964f454c7a5381d049dfd613075
00012670 <wiut00012670@gmail.com>
2023-11-04 16:23:11 +0500

    configure the DbContext and add conectionString

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,29 @@
+using API.DAL;
+using API.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure HTTPS redirection before building the app
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 7256; // Set HTTPS port
+});
+
+// Configure the DbContext 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB")));
 
 var app = builder.Build();
 
