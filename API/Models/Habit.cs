00f79951632047adc69061e9b508d7a6cdbea9ab
00012670 <wiut00012670@gmail.com>
2023-11-04 16:17:58 +0500

    delete WeatherForecast classes, add nuget packages and habit class

---
+++ API/Models/Habit.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Models
+{
+    public class Habit
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Frequency { get; set; }
+        public TargerFrequency Repeat { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public enum TargerFrequency
+        {
+            Daily,
+            Weekly,
+            Monthly
+        }
+    }
+}
