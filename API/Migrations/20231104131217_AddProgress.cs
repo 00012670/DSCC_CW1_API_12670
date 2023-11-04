3692dbb5c392fcd6e11924153228c029a2057798
00012670 <wiut00012670@gmail.com>
2023-11-04 18:19:42 +0500

    add progress migration

---
+++ API/Migrations/20231104131217_AddProgress.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class AddProgress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Progresses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HabitID = table.Column<int>(type: "int", nullable: true),
+                    HabitProgress = table.Column<int>(type: "int", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Progresses", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Progresses_Habits_HabitID",
+                        column: x => x.HabitID,
+                        principalTable: "Habits",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Progresses_HabitID",
+                table: "Progresses",
+                column: "HabitID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Progresses");
+        }
+    }
+}
