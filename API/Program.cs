64ea2d0513a71d7f4df2bea485a98bd8734afa8d
00012670 <wiut00012670@gmail.com>
2023-11-04 17:17:17 +0500

    add .gitignore file

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,6 @@
 using API.DAL;
 using API.Repositories;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
